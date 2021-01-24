27dd7e09264a7127fc9d992e876421a1ca6339f9
Xavier Maruff <62425409+Xavier-Maruff@users.noreply.github.com>
2021-01-24 23:06:55 +1100

    working function plotting, plus working scaling

--- src/image.hpp
+++ src/image.hpp
@@ -2,28 +2,98 @@
 #include <vector>
 #include <cmath>
 #include <array>
+#include <algorithm>
+#include <functional>
+#include "EasyBMP.hpp"
 
 using std::vector, std::array;
 
-typedef struct pixel {
+typedef struct point {
     int x, y;
+    point(int x_, int y_): x(x_), y(y_){}
+} point;
+
+typedef struct pixel {
+    point pos;
     array<int, 3> rgb = {0,0,0};
-    pixel(int x_, int y_): x(x_), y(y_){}
+    pixel(int x, int y): pos(x, y){}
 } pixel;
 
+typedef struct function {
+    std::function<int(int)> rule;
+    point rule_result;
+    int scale;
+
+    function(std::function<int(int)> rule_, int scale_ = 1):rule(rule_), rule_result(0,0), scale(scale_){}
+
+    void iterate(){
+        rule_result.x++;
+        rule_result.y = rule(rule_result.x)*scale;
+    }
+} function;
+
 namespace artemis{
+
     class image {
         public:
-        vector<vector<pixel>> pixels;
+        vector<vector<array<int, 3>>> pixels;
         int width, height;
 
         image(int width_, int height_): width(width_), height(height_) {
             for(int x = 0; x < width; x++){
+                vector<array<int, 3>> column = {};
                 for(int y = 0; y < height; y++){
-                    pixels.push_back(pixel(x, y));
+                    column.push_back(array<int, 3>({0,0,0}));
                 }
+                pixels.push_back(column);
             }
         }
+
+        void apply_function(function& lambda, array<int, 3> colour, point origin, point max, bool impactful = false, float scale = 1){
+            point rule_initial = lambda.rule_result;
+            lambda.rule_result = origin;
+            int previous = origin.y;
+
+            max.x = (max.x <= width ? max.x : width);
+            max.y = (max.y <= height ? max.y : height);
+
+            std::function<int(int, std::function<int(int)>, float)> scaled;
+            if(scale >= 1) {
+                scaled = [=](int val, std::function<int(int)> rule, float scale) -> int {return rule(val*scale);};
+            } else {
+                scaled = [=](int val, std::function<int(int)> rule, float scale) -> int {return (int)(rule(val)*scale);};
+            }
+
+            for(int x = 0; x < max.x; x++){
+                int result = scaled(x, lambda.rule, scale);
+                if(result >= max.y) {
+                    for(int y = previous; y < height; y++){
+                    pixels[x][height-y] = colour;
+                    }
+                    break;
+                }
+                else{
+                    for(int y = previous; y <= result; y++){
+                        pixels[x][height-y] = colour;
+                    }
+                }
+                previous = result;
+            }
+            if(!impactful) lambda.rule_result = rule_initial;
+        }
+
+        void render(EasyBMP::Image& img){
+            //todo: parallel implementation of this
+            for(int x = 0; x < width; x++){
+                for(int y = 0; y < height; y++){
+                    img.SetPixel(x, y, EasyBMP::RGBColor(pixels[x][y][0], pixels[x][y][1], pixels[x][y][2]));
+                }
+            }
+            return;
+            //img.SetPixel(x, y, EasyBMP::RGBColor(final_color, final_color, 0));
+        }
+
+
     };
 }
 
