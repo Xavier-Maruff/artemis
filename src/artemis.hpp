529231b0d77c7575f10b875bf69f939a6f236e2d
Xavier Maruff <62425409+Xavier-Maruff@users.noreply.github.com>
2021-01-25 11:08:50 +1100

    added more constructors

--- src/artemis.hpp
+++ src/artemis.hpp
@@ -11,22 +11,23 @@ using std::vector, std::array, std::string;
 
 typedef struct point {
     int64_t x, y;
-    point(int64_t x_, int64_t y_): x(x_), y(y_){}
+    point(const int64_t x_, const int64_t y_): x(x_), y(y_){}
 } point;
 
 class artemis : public EasyBMP::Image {
     public:
 
-    artemis(int width_, int64_t height_, string fname, array<uint8_t, 3> bg):
-    EasyBMP::Image(width_, height_, fname, EasyBMP::RGBColor({bg[0], bg[1], bg[2]}))
-    {}
+    artemis(const int64_t width_, const int64_t height_, const string& fname, const array<uint8_t, 3> bg):EasyBMP::Image(width_, height_, fname, EasyBMP::RGBColor({bg[0], bg[1], bg[2]})){}
+    artemis(const int64_t width_, const int64_t height_, const string& fname):EasyBMP::Image(width_, height_, fname){}
+    artemis(const int64_t width_, const int64_t height_, const array<uint8_t, 3> bg):EasyBMP::Image(width_, height_,  EasyBMP::RGBColor({bg[0], bg[1], bg[2]})){}
+    artemis(const int64_t width_, const int64_t height_):EasyBMP::Image(width_, height_){}
 
     ~artemis(){}
 
-    void render_function(std::function<int(int)> lambda, array<int, 3> colour, point origin, point max, float scale = 1){
+    void render_function(const std::function<int(int)> lambda, const array<int, 3> colour, const point origin, const point max, const float scale = 1){
 
         int64_t previous = (origin.y >= 0 ? (origin.y < height ? origin.y : height) : 0);
-        EasyBMP::RGBColor bmp_colour(colour[0], colour[1], colour[2]);
+        const EasyBMP::RGBColor bmp_colour(colour[0], colour[1], colour[2]);
         std::function<int64_t(int64_t)> scaled;
 
         if(scale >= 1) scaled = [&](int64_t val) -> int64_t {return lambda(val*scale);};
